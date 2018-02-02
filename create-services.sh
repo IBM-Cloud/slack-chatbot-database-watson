41a733f32852b0b4bd13a7d0e602a47de8173ee9
Henrik Loeser <data-henrik@users.noreply.github.com>
2018-02-02 14:03:12 +0100

    Updated service name

--- create-services.sh
+++ create-services.sh
@@ -1,3 +1,4 @@
 # Create the services
 bx service create dashDB Entry eventDB
+bx service key-create dashDB-p slackbotkey
 bx service create conversation free eventConversation
--- setup.sh
+++ setup.sh
@@ -3,8 +3,9 @@
 #
 # Written by Henrik Loeser
 
-# create Db2 Warehouse service
-# bx service create dashDB entry myEventDB
+# create Db2 Warehouse service and service credentials
+# bx service create dashDB entry eventDB
+# bx service key-create dashDB-p slackbotkey
 
 # create IBM Watson Conversation service
 # bx service create conversation free eventConversation
@@ -12,15 +13,15 @@
 # create package, install action for setup and invoke
 bx wsk package create slackdemo
 bx wsk action create slackdemo/db2Setup db2-setup.js  --kind nodejs:8
-bx wsk service bind dashDB slackdemo/db2Setup  --instance myEventDB
+bx wsk service bind dashDB slackdemo/db2Setup  --instance eventDB
 bx wsk action invoke slackdemo/db2Setup -p mode "[\"setup\"]" -r
 bx wsk action invoke slackdemo/db2Setup -p mode "[\"sampledata\"]" -r
 # action to fetch a single event by name
 bx wsk action create slackdemo/fetchEventByShortname eventFetch.js  --kind nodejs:8
-bx wsk service bind dashDB slackdemo/fetchEventByShortname --instance myEventDB
+bx wsk service bind dashDB slackdemo/fetchEventByShortname --instance eventDB
 # action to fetch a single event by dates
 bx wsk action create slackdemo/fetchEventByDates eventFetchDate.js  --kind nodejs:8
-bx wsk service bind dashDB slackdemo/fetchEventByDates  --instance myEventDB
+bx wsk service bind dashDB slackdemo/fetchEventByDates  --instance eventDB
 # action to insert a new event
 bx wsk action create slackdemo/eventInsert eventInsert.js  --kind nodejs:8
-bx wsk service bind dashDB slackdemo/eventInsert  --instance myEventDB
+bx wsk service bind dashDB slackdemo/eventInsert  --instance eventDB
