af9ec0a8de195c3c3a461c9e9b1a116270dd2d1c
Henrik Loeser <data-henrik@users.noreply.github.com>
2018-03-13 13:01:11 +0100

    Update create-services.sh

--- create-services.sh
+++ create-services.sh
@@ -1,4 +1,4 @@
 # Create the services
 bx service create dashDB Entry eventDB
-bx service key-create dashDB-p slackbotkey
+bx service key-create eventDB slackbotkey
 bx service create conversation free eventConversation
