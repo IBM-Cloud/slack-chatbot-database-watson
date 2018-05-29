aa2d538c73378a2236acb966b9b65e77039c0023
data-henrik <hloeser@de.ibm.com>
2018-05-29 13:57:52 +0200

    bx to ibmcloud

--- cleanup.sh
+++ cleanup.sh
@@ -3,11 +3,11 @@
 # Written by Henrik Loeser
 
 # Clean up database
-bx wsk action invoke slackdemo/db2Setup -p mode "[\"cleanup\"]" -r
+ibmcloud wsk action invoke slackdemo/db2Setup -p mode "[\"cleanup\"]" -r
 
 # Delete IBM Cloud Functions actions and package
-bx wsk action delete slackdemo/db2Setup
-bx wsk action delete slackdemo/fetchEventByShortname
-bx wsk action delete slackdemo/fetchEventByDates
-bx wsk action delete slackdemo/eventInsert
-bx wsk package delete slackdemo
+ibmcloud wsk action delete slackdemo/db2Setup
+ibmcloud wsk action delete slackdemo/fetchEventByShortname
+ibmcloud wsk action delete slackdemo/fetchEventByDates
+ibmcloud wsk action delete slackdemo/eventInsert
+ibmcloud wsk package delete slackdemo
--- setup.sh
+++ setup.sh
@@ -1,36 +1,36 @@
 # Automatically set up services and actions for tutorial on
-# Database-backed Slackbot with Watson conversation
+# Database-backed Slackbot with Watson Assistant (conversation)
 #
 # Written by Henrik Loeser
 
 # create Db2 Warehouse service and service credentials
-# bx service create dashDB Entry eventDB
-# bx service key-create eventDB slackbotkey
+# ibmcloud service create dashDB Entry eventDB
+# ibmcloud service key-create eventDB slackbotkey
 
-# create IBM Watson Conversation service
-# bx service create conversation free eventConversation
+# create IBM Watson Assistant (Conversation) service
+# ibmcloud service create conversation free eventConversation
 
 # create package
-bx wsk package create slackdemo
+ibmcloud wsk package create slackdemo
 
 # create action for setup using Node.js environment
-bx wsk action create slackdemo/db2Setup db2-setup.js  --kind nodejs:8
+ibmcloud wsk action create slackdemo/db2Setup db2-setup.js  --kind nodejs:8
 
 # bind action to Db2 credentials
-bx wsk service bind dashDB slackdemo/db2Setup  --instance eventDB
+ibmcloud wsk service bind dashDB slackdemo/db2Setup  --instance eventDB
 
 # invoke actions to create table, then insert sample data
-bx wsk action invoke slackdemo/db2Setup -p mode "[\"setup\"]" -r
-bx wsk action invoke slackdemo/db2Setup -p mode "[\"sampledata\"]" -r
+ibmcloud wsk action invoke slackdemo/db2Setup -p mode "[\"setup\"]" -r
+ibmcloud wsk action invoke slackdemo/db2Setup -p mode "[\"sampledata\"]" -r
 
 # action to fetch a single event by name
-bx wsk action create slackdemo/fetchEventByShortname eventFetch.js  --kind nodejs:8
-bx wsk service bind dashDB slackdemo/fetchEventByShortname --instance eventDB
+ibmcloud wsk action create slackdemo/fetchEventByShortname eventFetch.js  --kind nodejs:8
+ibmcloud wsk service bind dashDB slackdemo/fetchEventByShortname --instance eventDB
 
 # action to fetch a single event by dates
-bx wsk action create slackdemo/fetchEventByDates eventFetchDate.js  --kind nodejs:8
-bx wsk service bind dashDB slackdemo/fetchEventByDates  --instance eventDB
+ibmcloud wsk action create slackdemo/fetchEventByDates eventFetchDate.js  --kind nodejs:8
+ibmcloud wsk service bind dashDB slackdemo/fetchEventByDates  --instance eventDB
 
 # action to insert a new event
-bx wsk action create slackdemo/eventInsert eventInsert.js  --kind nodejs:8
-bx wsk service bind dashDB slackdemo/eventInsert  --instance eventDB
+ibmcloud wsk action create slackdemo/eventInsert eventInsert.js  --kind nodejs:8
+ibmcloud wsk service bind dashDB slackdemo/eventInsert  --instance eventDB
