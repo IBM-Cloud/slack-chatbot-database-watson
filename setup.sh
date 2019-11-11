cf2bd8d183054fe5f9528edd0a183657d498d85d
Moritz Hader <Moritz.Hader@de.ibm.com>
2019-11-11 17:13:30 +0100

    Fixed problem where setup script failed
    DB_Service paramter must be in quotes

--- setup.sh
+++ setup.sh
@@ -18,20 +18,20 @@ ibmcloud fn package create slackdemo
 ibmcloud fn action create slackdemo/db2Setup db2-setup.js  --kind nodejs:8
 
 # bind action to Db2 credentials
-ibmcloud fn service bind $DB_SERVICE slackdemo/db2Setup  --instance eventDB
+ibmcloud fn service bind "$DB_SERVICE" slackdemo/db2Setup  --instance eventDB
 
 # invoke actions to create table, then insert sample data
 ibmcloud fn action invoke slackdemo/db2Setup -p mode "[\"setup\"]" -r
 ibmcloud fn action invoke slackdemo/db2Setup -p mode "[\"sampledata\"]" -r
 
 # action to fetch a single event by name
-ibmcloud fn action create slackdemo/fetchEventByShortname eventFetch.js  --kind nodejs:8 --web true --web-secure $theSecret
-ibmcloud fn service bind $DB_SERVICE slackdemo/fetchEventByShortname --instance eventDB
+ibmcloud fn action create slackdemo/fetchEventByShortname eventFetch.js  --kind nodejs:8 --web true --web-secure "$theSecret"
+ibmcloud fn service bind "$DB_SERVICE" slackdemo/fetchEventByShortname --instance eventDB
 
 # action to fetch a single event by dates
-ibmcloud fn action create slackdemo/fetchEventByDates eventFetchDate.js  --kind nodejs:8 --web true --web-secure $theSecret
-ibmcloud fn service bind $DB_SERVICE slackdemo/fetchEventByDates  --instance eventDB
+ibmcloud fn action create slackdemo/fetchEventByDates eventFetchDate.js  --kind nodejs:8 --web true --web-secure "$theSecret"
+ibmcloud fn service bind "$DB_SERVICE" slackdemo/fetchEventByDates  --instance eventDB
 
 # action to insert a new event
-ibmcloud fn action create slackdemo/eventInsert eventInsert.js  --kind nodejs:8 --web true --web-secure $theSecret
-ibmcloud fn service bind $DB_SERVICE slackdemo/eventInsert  --instance eventDB
+ibmcloud fn action create slackdemo/eventInsert eventInsert.js  --kind nodejs:8 --web true --web-secure "$theSecret"
+ibmcloud fn service bind "$DB_SERVICE" slackdemo/eventInsert  --instance eventDB
